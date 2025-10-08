90806031e36b40af879c0d88c7222bbf5bd8ada7
Igor <igor.gds.00@gmail.com>
2025-10-08 18:45:57 -0300

    Cria componente summary

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -3,6 +3,7 @@ import { LinearGradient } from "expo-linear-gradient";
 import { colors } from "@/theme";
 import { styles } from "./styles";
 import Separator from "../Separator";
+import Summary from "../Summary";
 
 export type HomeHeaderProps = {
   total: string;
@@ -24,6 +25,18 @@ export default function HomeHeader({ data }: Props) {
       </View>
 
       <Separator color={colors.blue[400]} />
+
+      <View style={styles.summary}>
+        <Summary
+          data={{ label: "Entradas", value: "R$ 6.184,90" }}
+          icon={{ name: "arrow-upward", color: colors.green[500] }}
+        />
+        <Summary
+          data={{ label: "Saídas", value: "-R$ 883,65" }}
+          icon={{ name: "arrow-downward", color: colors.red[400] }}
+          isLeft
+        />
+      </View>
 
     </LinearGradient>
   );
