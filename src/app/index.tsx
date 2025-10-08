30ada52bd1f5b4068634cbed9f36c91cedde00b9
Igor <igor.gds.00@gmail.com>
2025-10-08 18:12:44 -0300

    Adiciona expo-linear-gradient na aplicação

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,25 +1,10 @@
-import { colors } from "@/theme/colors";
-import { fontFamily } from "@/theme/fontFamily";
-import { router } from "expo-router";
-import { Text, View, Button } from "react-native";
+import HomeHeader from "@/components/HomeHeader";
+import { View } from "react-native";
 
 export default function Index() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text style={{ fontFamily: fontFamily.bold, fontSize: 34 }}>Olá</Text>
-
-      <Button
-        title="Nova Meta"
-        onPress={() => router.navigate('/target')}
-      />
-      <Button
-        title="Transação"
-        onPress={() => router.navigate('/transaction/132')}
-      />
-      <Button
-        title="Progresso"
-        onPress={() => router.navigate('/in-progress/12')}
-      />
+    <View style={{ flex: 1 }}>
+      <HomeHeader />
     </View>
   )
 }
